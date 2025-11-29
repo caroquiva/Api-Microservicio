2de50566e2461de10ebd30413153ec925a092cbc
Carolina Quintero <caroquiva@gmail.com>
2025-11-29 18:24:22 -0500

    Implementación APIGATEWAY

---
+++ src/main/java/com/breaze/ApiGateway/ApiMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.breaze.ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/breaze/ApiGateway/ApiMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breaze.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
